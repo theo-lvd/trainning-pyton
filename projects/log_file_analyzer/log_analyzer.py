e60319c2219599a159ae67c0eda6d12fb1350056
Théo Levade <theo.sherco@gmail.com>
2025-09-26 17:05:28 +0200

    log analyzer project

---
+++ projects/log_file_analyzer/log_analyzer.py
@@ -0,0 +1,33 @@
+import os
+
+
+#TOOLS :
+
+def analyze_log_file(filename):
+    error_lines = []
+    with open(filename, "r") as sample_log :
+        log_raw = sample_log.read()
+        log_lines = log_raw.split("\n")
+
+        for lines in log_lines :
+            if "ERROR" in lines :
+                error_lines.append(lines)
+        return error_lines
+
+
+def write_error_report(filename, error_lines) :
+    with open(filename, "w") as error_report_log :
+        for lines in error_lines :
+            error_report_log.write(lines + "\n")
+
+
+#MAIN SCRIPT :
+
+log_file = "sample.log"
+error_report_file = "error_report.log"
+
+report = analyze_log_file(log_file)
+
+write_error_report(error_report_file, report)
+
+print("Report generated successfuly.")
