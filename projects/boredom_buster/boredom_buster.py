b7fbf525e315b1974d8db042105fba9a32219be7
Théo Levade <theo.sherco@gmail.com>
2025-10-10 14:07:08 +0200

    save

--- projects/boredom_buster/boredom_buster.py
+++ projects/boredom_buster/boredom_buster.py
@@ -5,7 +5,7 @@ response = requests.get("https://bored-api.appbrewery.com/random")
 if response.status_code == 200 :
     data = response.json()
     print("Request succesfull")
-    print(data["activity"])
+    print(f"Suggested activity: {data["activity"]}")
 
 else :
     print(f"Request error : {response.status_code} - {response.text}")
